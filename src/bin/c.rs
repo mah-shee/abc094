49db82c7328f8ccbd199c6f2a07fd8113e513850
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-20 23:05:31 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,16 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        x_ori : [usize; n]
+    }
+    let mut x = x_ori.clone();
+    x.sort();
+    for i in 0..n {
+        if x_ori[i] >= x[n / 2] {
+            println!("{} ", x[(n - 1) / 2]);
+        } else {
+            println!("{} ", x[n / 2]);
+        }
     }
 }
